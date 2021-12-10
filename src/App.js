820341f387b0e70dd838a1dd3e61e7448e3550fa
helloaarthy <helloaarthy@gmail.com>
2021-12-09 22:13:24 -0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,42 +3,75 @@ import axios from 'axios';
 import './style.css';
 
 export default function App() {
-  const [pokemon, setPokemon] = useState(0);
-  const [pokemonType, setPokemonType] = useState(0);
+  const [pokemon1, setPokemon1] = useState(0);
+  const [pokemonType1, setPokemonType1] = useState(0);
+  const [pokemon2, setPokemon2] = useState(0);
+  const [pokemonType2, setPokemonType2] = useState(0);
 
   useEffect(() => {
-    fetchPokemon();
+    fetchPokemon1();
   }, []);
 
-  const random = Math.floor(Math.random() * 721);
+  useEffect(() => {
+    fetchPokemon2();
+  }, []);
 
-  const fetchPokemon = () => {
+  const random1 = Math.floor(Math.random() * 721);
+  const random2 = Math.floor(Math.random() * 721);
+
+  const fetchPokemon1 = () => {
     axios
-      .get('https://pokeapi.co/api/v2/pokemon/' + random)
+      .get('https://pokeapi.co/api/v2/pokemon/' + random1)
       .then((response) => {
-        setPokemon(response.data);
-        setPokemonType(response.data.types[0].type);
+        setPokemon1(response.data);
+        setPokemonType1(response.data.types[0].type);
+      });
+  };
+
+  const fetchPokemon2 = () => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/' + random2)
+      .then((response) => {
+        setPokemon2(response.data);
+        setPokemonType2(response.data.types[0].type);
       });
   };
 
   return (
-    <div className={`background-${pokemonType.name}`}>
-      <p className="text">
-        Here is your pokemon{' '}
-        <em>
-          <b class="pokemonName"> {pokemon.name} </b>
-        </em>
-        of type {pokemonType.name}
-      </p>
-      <img
-        id="pokemonImg"
-        src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}
-        width="300"
-        height="300"
-      />
-      <button id="clickMe" onClick={fetchPokemon}>
-        Click Me!
-      </button>
+    <div>
+      <div className={`background-${pokemonType1.name}`}>
+        <p className="text">
+          Here is your pokemon{' '}
+          <em>
+            <b className="pokemonName1"> {pokemon1.name} </b>
+          </em>
+          of type <b className="pokemonTypeName1"> {pokemonType1.name} </b>
+        </p>
+        <img
+          id="pokemonImg1"
+          src={`https://img.pokemondb.net/artwork/large/${pokemon1.name}.jpg`}
+          width="300"
+          height="300"
+        />
+        <div className={`background-${pokemonType2.name}`}>
+          <p className="text">
+            Here is your pokemon{' '}
+            <em>
+              <b className="pokemonName1"> {pokemon2.name} </b>
+            </em>
+            of type <b className="pokemonTypeName1"> {pokemonType2.name} </b>
+          </p>
+          <img
+            id="pokemonImg1"
+            src={`https://img.pokemondb.net/artwork/large/${pokemon2.name}.jpg`}
+            width="300"
+            height="300"
+          />
+          <button id="clickMe" onClick={(fetchPokemon1, fetchPokemon2)}>
+            Click Me!
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
